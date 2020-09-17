78848cf90e29052d82140d5adecb099f4cf5e634
connorvanderveen <816535@mystma.org>
2020-09-17 20:14:12 +0000

    top 10 movies

---
+++ Unit1/src/app/App.java
@@ -0,0 +1,9 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("1. joker");
+    System.out.println("2. deadpool");
+System.out.println("3. toy story");
+System.out.println("4.");}
+}

85c23ada7f9bc007632d94b89c3d2e584319cb44
connorvanderveen <816535@mystma.org>
2020-09-24 19:54:20 +0000

    user input madlid

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,9 +1,21 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("1. joker");
-    System.out.println("2. deadpool");
-System.out.println("3. toy story");
-System.out.println("4.");}
+    Scanner input = new Scanner(System.in);
+    System.out.println("please enter you first name");
+String firstname = input.nextLine();
+System.out.println("please enter last name");                                               
+String lastname = input.nextLine();
+System.out.println("please enter age");
+String age = input.nextLine();
+System.out.println("please enter your grade");
+String grade = input.nextLine();
+System.out.println("please enter your favorite color");
+String color = input.nextLine();
+System.out.println("hello" + firstname + lastname + " you are in grade " + grade + "  and your favorite color is " + color);
+
+        }
 }

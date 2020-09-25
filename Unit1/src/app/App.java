bbb2b5052f1a2518bde51f04ba7843a1f87c0961
connorvanderveen <816535@mystma.org>
2020-09-25 19:56:25 +0000

    count

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,21 +1,30 @@
 package app;
 
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
-    Scanner input = new Scanner(System.in);
-    System.out.println("please enter you first name");
-String firstname = input.nextLine();
-System.out.println("please enter last name");                                               
-String lastname = input.nextLine();
-System.out.println("please enter age");
-String age = input.nextLine();
-System.out.println("please enter your grade");
-String grade = input.nextLine();
-System.out.println("please enter your favorite color");
-String color = input.nextLine();
-System.out.println("hello" + firstname + lastname + " you are in grade " + grade + "  and your favorite color is " + color);
-
+        int count = 0;
+        while (count < 10) {
+            System.out.println("the count is " + count);
+            count = count + 1;
         }
+        System.out.println("count finished");
+        {
+ int count2= 0;
+            while (count2 < 21);
+            System.out.println("the count is " + count2);
+        count2 = count2 + 2;
+    }
+        
+
+        System.out.println("count is finished");
+
+    {
+        int count3= 0;
+        while (count3 < 31)
+    System.out.println("the count is " + count);
+    count3 = count3 + 5;
+    System.out.println("the count is finishd");
+    }
+    
+    }
 }

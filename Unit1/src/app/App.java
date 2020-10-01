9d501c1962a6c72eca8ed014c9a08587e670cb18
connorvanderveen <816535@mystma.org>
2020-10-01 20:05:33 +0000

    quiz

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,29 +2,15 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int count = 0;
-        while (count < 10) {
-            System.out.println("the count is " + count);
-            count = count + 1;
+
+        int myint = 10;
+
+        if (myint % 2 == 0) {
+            System.out.println(myint + " is an even number");
+        } else {
+            System.out.println(myint + " is an odd number");
         }
-        System.out.println("count finished");
-        {
- int count2= 0;
-            while (count2 < 21);
-            System.out.println("the count is " + count2);
-        count2 = count2 + 2;
-    }
-        
+        System.out.println("all finished");
 
-        System.out.println("count is finished");
-
-    {
-        int count3= 0;
-        while (count3 < 31)
-    System.out.println("the count is " + count);
-    count3 = count3 + 5;
-    System.out.println("the count is finishd");
-    }
-    
     }
 }

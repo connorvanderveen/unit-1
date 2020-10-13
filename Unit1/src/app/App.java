753452eace065232a325799c72b0290091fa871f
connorvanderveen <816535@mystma.org>
2020-10-13 19:42:52 +0000

    numbers

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,16 +1,18 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-int answer = 22;
-if (answer < 22);{
-System.out.println("above ");
-}
-if (answer > 22); {
-    System.out.println("below");
-}
-if (answer == 22); {
-    System.out.println("correct");
-}
+        Scanner input = new Scanner(System.in);
+System.out.println("please enter a number.");
+String name = input.nextLine();
+
+
+System.out.println("please enter another number.");
+String number = input.nextLine();
+
+System.out.println("the numbers are " + name + number);
+
     }
 }

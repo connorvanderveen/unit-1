e1c88d12616b49226d72533e76149573be06ef12
connorvanderveen <816535@mystma.org>
2020-10-22 19:45:07 +0000

    challenge 4

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -4,15 +4,25 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
-System.out.println("please enter a number.");
-String name = input.nextLine();
+   Scanner input = new Scanner(System.in);
+   
+   System.out.println("please enter a number");
+   int num1 = input.nextInt();
+   input.nextLine();
 
+System.out.println("please enter a larger number");
+   int num2 = input.nextInt();
+   input.nextLine();
 
-System.out.println("please enter another number.");
-String number = input.nextLine();
+   num1++;
 
-System.out.println("the numbers are " + name + number);
-
+   while (num1  < num2){
+    
+    if (num1 % 2 == 0){
+    
+    System.out.println(num1 );
+          }
+                num1++;
+   }
     }
 }

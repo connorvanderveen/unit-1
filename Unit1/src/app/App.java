42529e7aa573f083e392156f77f92247f49fa53b
connorvanderveen <816535@mystma.org>
2020-11-06 20:34:54 +0000

    public static void

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,28 +1,25 @@
 package app;
 
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
-   Scanner input = new Scanner(System.in);
-   
-   System.out.println("please enter a number");
-   int num1 = input.nextInt();
-   input.nextLine();
+helloworld();
+connor();
+kort();
+vanderveen();
 
-System.out.println("please enter a larger number");
-   int num2 = input.nextInt();
-   input.nextLine();
-
-   num1++;
-
-   while (num1  < num2){
-    
-    if (num1 % 2 == 0){
-    
-    System.out.println(num1 );
-          }
-                num1++;
-   }
     }
+
+public static void helloworld(){
+    System.out.println("hello world");
+}
+public static void connor(){
+    System.out.println("connor");
+}
+public static void kort(){
+    System.out.println("kort");
+}
+public static void vanderveen(){
+    System.out.println("vanderveen");
+}
+
 }

f011e052d47916ba7c9025fad56c806a3fbfbab1
connorvanderveen <816535@mystma.org>
2020-11-13 16:54:17 +0000

    challenge 3

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,22 +3,24 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 Scanner input = new Scanner(System.in);
-String password = "coolguy";
-int tries = 0;
-boolean secretMessageGiven = false;
 
-while (tries < 3){
-    if (secretMessageGiven == false){
-System.out.println(" enter the password");
-String enteredPassword = input.nextLine();
-
-if (enteredPassword.equals(password)){
-    System.out.println(" correct ");
-    System.out.println(" have you heard of nord vpn ");
+int targetNum =25;
+boolean isFinished = false;
+while (isFinished == false){
+System.out.println("choose a number 0-100");
+int guess = input.nextInt();
+input.nextLine();
+if (guess == targetNum){
+    System.out.println("correct");
+    isFinished = true;
 }
-    }
+else if (guess < targetNum){
+    System.out.println("low");
 }
-
+else{
+    System.out.println("high");
+}
+}
 
     }
 }
